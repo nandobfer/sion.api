2e6bef7676b0a82f884ad57344b70a6ffa0679ed
nandobfer <nandobfer@yahoo.com.br>
2023-06-22 15:59:54 -0300

    update no gui env

--- src/whatsapp.ts
+++ src/whatsapp.ts
@@ -3,6 +3,9 @@ import { Client, LocalAuth } from "whatsapp-web.js"
 
 export const whatsapp = new Client({
     authStrategy: new LocalAuth({ dataPath: "whatsapp.auth" }),
+    puppeteer: {
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    },
 })
 
 whatsapp.on("qr", (qr) => {
