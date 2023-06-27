439d9e41b67e976ab2f41b8ddd7395432bb0c6f9
nandobfer <nandobfer@yahoo.com.br>
2023-06-27 17:30:12 -0300

    db: resync and added role to users

--- prisma/migrations/20230627202959_users_role/migration.sql
+++ prisma/migrations/20230627202959_users_role/migration.sql
@@ -2,17 +2,19 @@
 DROP INDEX `contracts_seller_id_fkey` ON `contracts`;
 
 -- DropIndex
+DROP INDEX `images_user_id_fkey` ON `images`;
+
+-- DropIndex
 DROP INDEX `logs_contract_id_fkey` ON `logs`;
 
 -- DropIndex
 DROP INDEX `logs_seller_id_fkey` ON `logs`;
 
+-- DropIndex
+DROP INDEX `texts_user_id_fkey` ON `texts`;
+
 -- AlterTable
-ALTER TABLE `users` ADD COLUMN `address` VARCHAR(191) NULL,
-    ADD COLUMN `cep` VARCHAR(191) NULL,
-    ADD COLUMN `district` VARCHAR(191) NULL,
-    ADD COLUMN `number` VARCHAR(191) NULL,
-    ADD COLUMN `rg` VARCHAR(191) NULL;
+ALTER TABLE `users` ADD COLUMN `role` VARCHAR(191) NOT NULL DEFAULT 'client';
 
 -- AddForeignKey
 ALTER TABLE `contracts` ADD CONSTRAINT `contracts_seller_id_fkey` FOREIGN KEY (`seller_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -31,3 +33,9 @@ ALTER TABLE `logs` ADD CONSTRAINT `logs_contract_id_fkey` FOREIGN KEY (`contract
 
 -- AddForeignKey
 ALTER TABLE `logs` ADD CONSTRAINT `logs_seller_id_fkey` FOREIGN KEY (`seller_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `texts` ADD CONSTRAINT `texts_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `images` ADD CONSTRAINT `images_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
