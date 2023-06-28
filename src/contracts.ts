c6940bc8b8788ff5fb2a465a720673d60d543400
nandobfer <nandobfer@yahoo.com.br>
2023-06-28 16:36:40 -0300

    update: including status on contract query

--- src/contracts.ts
+++ src/contracts.ts
@@ -4,20 +4,26 @@ const router = express.Router()
 const prisma = new PrismaClient()
 
 router.get("/", async (request: Request, response: Response) => {
-    const contracts = await prisma.contracts.findMany({ include: { seller: true } })
+    const contracts = await prisma.contracts.findMany({ include: { seller: true, status: true } })
     response.json(contracts)
 })
 
 router.post("/id", async (request: Request, response: Response) => {
     const data = request.body
-    const contract = await prisma.contracts.findUnique({ where: { id: Number(data.id) }, include: { seller: true } })
+    const contract = await prisma.contracts.findUnique({
+        where: { id: Number(data.id) },
+        include: { seller: true, status: true },
+    })
 
     response.json(contract)
 })
 
 router.post("/seller", async (request: Request, response: Response) => {
     const data = request.body
-    const contracts = await prisma.contracts.findMany({ where: { seller_id: Number(data.id) }, include: { seller: true } })
+    const contracts = await prisma.contracts.findMany({
+        where: { seller_id: Number(data.id) },
+        include: { seller: true, status: true },
+    })
 
     response.json(contracts)
 })
@@ -27,7 +33,7 @@ router.post("/search", async (request: Request, response: Response) => {
 
     const contracts = await prisma.contracts.findMany({
         where: { name: { contains: data.search.trim() } },
-        include: { seller: true },
+        include: { seller: true, status: true },
     })
 
     response.json(contracts)
