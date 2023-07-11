40ddbc7243fb8c311ce700b7a6623680a3dc6091
nandobfer <nandobfer@yahoo.com.br>
2023-07-11 19:46:30 -0300

    feat: added boards broadcast

---
+++ src/io/boards.ts
@@ -0,0 +1,21 @@
+import { boards } from "@prisma/client"
+import { Socket } from "socket.io"
+
+export const handleBoards = (socket: Socket) => {
+    console.log(`new connection: ${socket.id}`)
+
+    socket.on("board:new", (board: boards) => {
+        console.log(board)
+        socket.broadcast.emit("board:new", board)
+    })
+
+    socket.on("board:update", (board: boards) => {
+        console.log(board)
+        socket.broadcast.emit("board:update", board)
+    })
+
+    socket.on("board:remove", (board: boards) => {
+        console.log(board)
+        socket.broadcast.emit("board:remove", board)
+    })
+}
