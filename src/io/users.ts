a781caca8ecf1efb68df802aa4b6fd27e78b1aa0
nandobfer <nandobfer@yahoo.com.br>
2023-07-13 15:11:45 -0300

    feat: saving userLog of user:update

--- src/io/users.ts
+++ src/io/users.ts
@@ -1,5 +1,7 @@
-import { users } from "@prisma/client"
+import { PrismaClient, users } from "@prisma/client"
 import { Socket } from "socket.io"
+
+const prisma = new PrismaClient()
 
 export const handleUsers = (socket: Socket) => {
     socket.on("user:new", (user: users) => {
@@ -7,9 +9,19 @@ export const handleUsers = (socket: Socket) => {
         socket.broadcast.emit("user:new", user)
     })
 
-    socket.on("user:update", (user: users) => {
-        console.log(user)
-        socket.broadcast.emit("user:update", user)
+    socket.on("user:update", async (data: { user: users; seller: users }) => {
+        const user = data.user
+        const seller = data.seller
+
+        socket.broadcast.emit("user:update", seller)
+
+        await prisma.userLogs.create({
+            data: {
+                date: new Date(),
+                text: `Usuário ${seller.name} atualizado`,
+                user_id: user.id,
+            },
+        })
     })
 
     socket.on("user:remove", (user: users) => {
